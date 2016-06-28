01fe531460afe2848c3a3de9c2c96881ae58197c
vmora <vincent.mora@oslandia.com>
2016-06-28 14:13:29 +0200

    basic infra

---
+++ docs/build.py
@@ -0,0 +1,87 @@
+# coding=utf-8
+"""
+packaging script for the qgis_section documentation
+
+USAGE
+    build.py [-h]
+
+OPTIONS
+    -h, --help
+        print this help
+
+"""
+
+import os
+import getopt
+import sys
+import shutil
+import shlex
+from subprocess import Popen, PIPE
+from  ..utility import package
+from  ..database.database_doc import DatabaseDocumentation
+
+def create_package(build_dir):
+    zip_filename = os.path.join(build_dir, "qgis_section.zip")
+    package.zip_(zip_filename)
+    package.install(build_dir, zip_filename)
+
+if __name__ == "__main__":
+
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:],
+                "h",
+                ["help"])
+    except Exception as e:
+        sys.stderr.write(str(e)+"\n")
+        exit(1)
+
+    optlist = dict(optlist)
+
+    if "-h" in optlist or "--help" in optlist:
+        help(sys.modules[__name__])
+        exit(0)
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    build_dir = os.path.join(current_dir, "build")
+    html_dir = os.path.join(build_dir, "html")
+    source_dir =  os.path.join(current_dir, "source")
+    doctrees_dir = os.path.join(build_dir, "doctrees")
+    module_dir = os.path.join(build_dir, "qgis_section")
+    api_dir = os.path.join(source_dir, "api")
+    static_dir = os.path.join(source_dir, "_static")
+
+    def exec_cmd(cmd):
+        print " ".join(cmd) 
+        out, err = Popen(cmd, 
+                stderr=PIPE, stdout=PIPE).communicate()
+        if err :
+            print "failed"
+            print err
+            exit(1)
+
+    if not os.path.isdir(build_dir):
+        os.mkdir(build_dir)
+
+    if not os.path.isdir(static_dir):
+        os.mkdir(static_dir)
+
+    if os.path.isdir(api_dir):
+        shutil.rmtree(api_dir)
+    os.mkdir(api_dir)
+
+    if not os.path.isdir(html_dir):
+        os.mkdir(html_dir)
+
+    if not os.path.isdir(os.path.join(html_dir, 'model')):
+        os.mkdir(os.path.join(html_dir, 'model'))
+    shutil.copyfile(os.path.join(source_dir, 'model', 'vue_ensemble.svg'), os.path.join(html_dir, 'model', 'vue_ensemble.svg'))
+
+    create_package(build_dir)
+
+    open(os.path.join(source_dir, "generated_er_graph.rst"), "w").write(
+            "ER Graph\n########\n\n.. graphviz::\n\n"+DatabaseDocumentation().dot())
+
+    exec_cmd(shlex.split("sphinx-apidoc -e -d2 -T -o")+[api_dir, module_dir, "**/*_test.py", "*_demo.py"])
+    exec_cmd(shlex.split("sphinx-build -b html -d")+[doctrees_dir, source_dir, html_dir])
+
+
