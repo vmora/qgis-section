6e33a3b97425d27bc514cafb7a92d8003090188a
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2016-07-26 14:08:38 +0200

    Fixed line selection

--- plugin.py
+++ plugin.py
@@ -28,17 +28,21 @@ class LineSelectTool(QgsMapTool):
     line_clicked = pyqtSignal(str, int)
     def __init__(self, canvas):
         QgsMapTool.__init__(self, canvas)
+        self.canvas = canvas
 
     def canvasReleaseEvent(self, event):
         print "canvasReleaseEvent"
         #Get the click
-        radius = QgsMapTool.searchRadiusMU(self.canvas())
+        radius = QgsMapTool.searchRadiusMU(self.canvas)
         x = event.pos().x()
         y = event.pos().y()
-        for layer in self.canvas().layers():
+        for layer in self.canvas.layers():
+
+            layerPoint = self.toLayerCoordinates(layer, event.pos())
             if layer.type() == QgsMapLayer.VectorLayer and layer.geometryType() == QGis.Line:
+
                 for feat in layer.getFeatures(QgsFeatureRequest(
-                    QgsRectangle(x-radius, y-radius, x+radius, y+radius))):
+                    QgsRectangle(layerPoint.x() - radius, layerPoint.y() - radius, layerPoint.x() + radius, layerPoint.y() + radius))):
                     self.line_clicked.emit(layer.id(), feat.id())
                     return
         self.line_clicked.emit(None, None)
@@ -53,7 +57,7 @@ class Plugin():
 
         self.toolbar = QToolBar()
         self.toolbar.addAction('select line').triggered.connect(self.set_section_line)
-        self.buffer_width = QLineEdit("10")
+        self.buffer_width = QLineEdit("0.5")
         self.buffer_width.setMaximumWidth(50)
         self.toolbar.addWidget(QLabel("Width:"))
         self.toolbar.addWidget(self.buffer_width)
@@ -63,8 +67,8 @@ class Plugin():
         self.old_tool = None
 
         # 2154 just for the fun, we don't care as long as the unit is meters
-        layer = QgsVectorLayer("Point?crs=epsg:2154&field=id:integer&field=name:string(20)&index=yes", 
-                "temporary_points", 
+        layer = QgsVectorLayer("Point?crs=epsg:2154&field=id:integer&field=name:string(20)&index=yes",
+                "temporary_points",
                 "memory")
         layer.beginEditCommand("test")
         provider = layer.dataProvider()
@@ -84,7 +88,7 @@ class Plugin():
         canvas.setLayerSet([QgsMapCanvasLayer(layer)])
 
         canvas.setCurrentLayer(layer)
-        canvas.zoomToFullExtent() 
+        canvas.zoomToFullExtent()
         canvas.setWheelAction(QgsMapCanvas.WheelZoomToMouseCursor)
 
         self.canvas_dock = QDockWidget('Qgis section other')
@@ -96,7 +100,7 @@ class Plugin():
         self.tool = None
         self.__map_tool_changed(self.iface.mapCanvas().mapTool())
         self.iface.mapCanvas().mapToolSet.connect(self.__map_tool_changed)
-    
+
     def __map_tool_changed(self, map_tool):
         if isinstance(map_tool, QgsMapToolPan):
             self.tool = QgsMapToolPan(self.canvas)
@@ -104,8 +108,10 @@ class Plugin():
         elif isinstance(map_tool, QgsMapToolZoom):
             self.tool = QgsMapToolZoom(self.canvas, map_tool.action().text().find(u"+") == -1)
             self.canvas.setMapTool(self.tool)
+        elif isinstance(map_tool, LineSelectTool):
+            pass
         else:
-            print 'map_tool', map_tool, map_tool.objetName()
+            print 'map_tool', map_tool
             self.canvas.setMapTool(None)
             self.tool = None
 
@@ -119,18 +125,40 @@ class Plugin():
         self.iface.mapCanvas().mapToolSet[QgsMapTool].disconnect()
 
     def set_section_line_(self, layer_id, feature_id):
-        print "selected", layer_id, feature_id
+        print "SelecteD", layer_id, feature_id
         line = None
         #self.canvas.set_section_line(line, float(self.buffer_width.text()))
-        self.iface.mapCanvas().setMapTool(self.old_tool)
         self.tool.line_clicked.disconnect()
         self.tool.setParent(None)
         self.tool = None
+        self.iface.mapCanvas().setMapTool(self.old_tool)
         self.oldtool = None
+
+        layer = QgsMapLayerRegistry.instance().mapLayer(layer_id)
+        for f in layer.getFeatures(QgsFeatureRequest(feature_id)):
+            geom = f.geometry()
+            buffer = geom.buffer(float(self.buffer_width.text()), 4)
+
+            # debug visu
+            # ___layer = QgsVectorLayer("polygon?crs=epsg:2154&field=id:integer&field=name:string(20)&index=yes",
+            #         "temporary_poly",
+            #         "memory")
+            # ___layer.beginEditCommand("test")
+            # provider = ___layer.dataProvider()
+            # fet = QgsFeature()
+            # fet.setGeometry(buffer)
+            # fet.setAttributes([1, "Johny"])
+            # provider.addFeatures([fet])
+            # ___layer.endEditCommand()
+            # QgsMapLayerRegistry.instance().addMapLayer(___layer, False)
+
+            # self.iface.mapCanvas().setLayerSet([QgsMapCanvasLayer(___layer)])
+            # self.iface.mapCanvas().refreshAllLayers()
+
+            print 'done'
 
     def set_section_line(self):
         print "set_section_line"
-
         self.old_tool = self.iface.mapCanvas().mapTool()
         self.tool = LineSelectTool(self.iface.mapCanvas())
         self.tool.line_clicked.connect(self.set_section_line_)
